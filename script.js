1c27c4932f806c024ae06fdc337eee7d07b3e575
Hugo <122971194+hugogilt@users.noreply.github.com>
2024-10-31 10:35:41 +0100

    Adapto animación de imagen para móviles

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const image = document.getElementById('bouncingImage');
+const jobs = document.querySelectorAll('#trabajos a');
+const flipCard = document.querySelector('.flip-card')
+const flipCardInner = document.querySelector('.flip-card-inner');
 let bounceInterval;
-let houverMaked;
-let jumping;
+
+
+window.onload = () => {
+  checkHoverSupport();
+  startBouncing();
+};
+window.addEventListener('resize', checkHoverSupport);
+
+
 // Función para hacer "saltito"
 function startBouncing() {
   bounceInterval = setInterval(() => {
@@ -10,28 +20,37 @@ function startBouncing() {
       image.style.animation = ''; // Elimina la animación después de que termina
     }, 500);
   }, 3000);
-  jumping = true;
 }
-// Detener el "saltito" al hacer hover
-image.addEventListener('mouseover', () => {
-  clearInterval(bounceInterval);
-  image.style.animation = '';
-  houverMaked = true;
-});
 
-// Función para verificar soporte de hover y manejar el "saltito"
+
+
 function checkHoverSupport() {
-  if (window.matchMedia('(hover: hover)').matches && !houverMaked) {
-    if (!jumping) {
-      startBouncing();
+  if (window.matchMedia('(hover: hover)').matches) { //SI ESTÁ EN UN ORDENADOR
+    for (let job of jobs) {
+      job.style.setProperty('--width-before', '100%');
+      job.style.removeProperty('color')
     }
+    // Detener el "saltito" al hacer mouseover
+    image.addEventListener('mouseover', () => {
+      clearInterval(bounceInterval);
+      image.style.animation = '';
+    });
+    image.addEventListener('click', () => {});
+    document.body.classList.remove("movil");
+    flipCardInner.classList.remove("clicked");
+
   } else {
-    clearInterval(bounceInterval);
-    image.style.animation = '';
-    jumping = false;
+    for (let job of jobs) {
+      job.style.setProperty('--width-before', '0%');
+      job.style.color = 'black';
+    }
+    // Detener el "saltito" al hacer click
+    flipCard.addEventListener('click', () => {
+      clearInterval(bounceInterval);
+      image.style.animation = '';
+      flipCardInner.classList.toggle('clicked');
+    });
+    image.addEventListener('mouseover', () => {});
+    document.body.classList.add("movil");
   }
 }
-
-// Inicia la verificación al cargar la página
-window.onload = checkHoverSupport;
-window.addEventListener('resize', checkHoverSupport);
